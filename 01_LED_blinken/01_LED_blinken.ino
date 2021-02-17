537acced6cc144baccb3801d5b033a18311aaa63
Adrian Kaiser <adrian@kaiser.pt>
2021-02-17 14:43:07 +0100

    Update 01_LED_blinken.ino
    added comments

--- 01_LED_blinken/01_LED_blinken.ino
+++ 01_LED_blinken/01_LED_blinken.ino
@@ -1,10 +1,10 @@
-// Sobald man das Arduino mit Strom versorgt oder reset drückt wird die setup Fkt. einmal ausgeführt
+// Sobald man das Arduino mit Strom versorgt oder reset drückt wird die setup Fkt. einmal ausgeführt.
 void setup() {
-  // LED_BUILTIN wird als Ausgang initialisiert
+  // LED_BUILTIN wird als Output initialisiert.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// Die Loop Fkt. wird als Endlosschleife ausgeführt 
+// Die Loop Fkt. wird als Endlosschleife ausgeführt. 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // LED einschalten (HIGH, da Spannung anliegt)
   delay(1000);                       // 1 Sekunde warten
